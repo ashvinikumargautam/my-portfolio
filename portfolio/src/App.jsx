f45735b13a43e72b2407b3701a28ab412f2c49c9
ashvini kumar gautam <ashvini1275@gmail.com>
2026-09-09 23:34:12 +0530

    path correct

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,7 +9,7 @@ import Education from './components/Education'
 import Certifications from './components/Certifications'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import achievements from './components/achievements'
+import Achievements from './components/achievements'
 export default function App() {
   return (
     <div className="relative min-h-screen overflow-x-hidden">
@@ -23,7 +23,7 @@ export default function App() {
         <Projects />
         <Education />
         <Certifications />
-        <achievements />
+        <Achievements />
         <Contact />
       </main>
       <Footer />
